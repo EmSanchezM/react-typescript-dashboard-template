2054e2608be8109816f4ed787cb61d6e83fbb4fc
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2023-08-04 14:28:24 -0600

    fix: format code

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,12 +1,12 @@
 const Sidebar = () => {
-	return (
-		<aside className='sidebar'>
-			<div className='logo__details'>
-				<i>icon</i>
-				<span className='logo__title'>Clinpays</span>
-			</div>
-		</aside>
-	);
+  return (
+    <aside className='sidebar'>
+      <div className='logo__details'>
+        <i>icon</i>
+        <span className='logo__title'>Clinpays</span>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
